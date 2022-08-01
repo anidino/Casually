0d221ba502984a638ba4a8ba95b16a445f7e1d22
AniElaina <ani.elaina.carr@gmail.com>
2022-08-01 15:06:53 -0400

    scrolling text box added, media queries adjusted.

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,7 @@ function currentSlide(n) {
 function SlideShow(n) {
   var i;
   var slides = document.getElementsByClassName("Containers");
-  var circles = document.getElementsByClassName("dots");
+  // var circles = document.getElementsByClassName("dots");
   if (n > slides.length) {
     slidePosition = 1;
   }
@@ -24,11 +24,11 @@ function SlideShow(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < circles.length; i++) {
-    circles[i].className = circles[i].className.replace(" enable", "");
-  }
+  // for (i = 0; i < circles.length; i++) {
+  //   circles[i].className = circles[i].className.replace(" enable", "");
+  // }
   slides[slidePosition - 1].style.display = "block";
-  circles[slidePosition - 1].className += " enable";
+  // circles[slidePosition - 1].className += " enable";
 }
 
 //TIMER FOR SLIDESHOW
